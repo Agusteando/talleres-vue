c469dff6baa591b736cf69ef80f5d40469303c33
Agusteando <desarrollo.tecnologico@casitaiedis.edu.mx>
2026-03-17 12:00:39 -0600

    worker fix!

---
+++ public/ngsw-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+
+    const clients = await self.clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    });
+
+    await self.registration.unregister();
+
+    for (const client of clients) {
+      client.navigate(client.url);
+    }
+  })());
+});
+
+self.addEventListener('fetch', () => {});
