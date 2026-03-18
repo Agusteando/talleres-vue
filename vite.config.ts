83edc5cb1ea8f4197d820c60ab51811739763976
Agusteando <desarrollo.tecnologico@casitaiedis.edu.mx>
2026-03-18 11:10:38 -0600

    ipad support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'iOS >= 12', 'Safari >= 12'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
+  build: {
+    target: ['es2015', 'safari12'],
+  },
 })
